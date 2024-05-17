28256265a5de46b96531b782a389ad30ecbcd423
yelcat <yelcat@gmail.com>
2024-05-18 00:29:17 +0800

    feat: add list view for chat

--- cmd/chat.go
+++ cmd/chat.go
@@ -6,13 +6,17 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"log/slog"
 	"os"
 	"strings"
 
 	"github.com/BurntSushi/toml"
+	"github.com/atotto/clipboard"
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/textarea"
-	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 
@@ -20,146 +24,215 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type chatUI struct {
-	textarea    textarea.Model
-	err         error
-	senderStyle lipgloss.Style
-	viewport    viewport.Model
-	messages    []string
+type resultMsg struct {
+	data  string // message recevied
+	index int    // message index number
 }
 
-func initChatUI() *chatUI {
+type model struct {
+	textarea    textarea.Model
+	err         error
+	respChan    chan resultMsg
+	senderStyle lipgloss.Style
+	messageList list.Model
+	spinner     spinner.Model
+}
+
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+type message struct {
+	role string // 消息发送者的 j角色
+	desc string // 消息内容
+}
+
+func (m message) Role() string { return m.role }
+func (m message) Title() string {
+	switch m.role {
+	case openai.ChatMessageRoleAssistant:
+		return "🤖: "
+	case openai.ChatMessageRoleUser:
+		return "👤: "
+	default:
+		return ""
+	}
+}
+func (m message) Description() string { return m.desc }
+func (m message) FilterValue() string {
+	return m.desc
+}
+
+func initialModel(respChan chan resultMsg) *model {
 	ta := textarea.New()
 	ta.Placeholder = "Send a message..."
 	ta.Focus()
 
 	ta.Prompt = "┃ "
-	ta.CharLimit = 280
+	ta.CharLimit = 2800
 
-	ta.SetWidth(30)
-	ta.SetHeight(3)
+	ta.SetWidth(400)
+	ta.SetHeight(4)
 
 	// Remove cursor line styling
 	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
-
 	ta.ShowLineNumbers = false
-
-	vp := viewport.New(300, 50)
-	vp.SetContent(fmt.Sprintf(`Welcome to the ChatGPT(%s)!
-Type a message and press Enter to send.`, conf.Model))
-
 	ta.KeyMap.InsertNewline.SetEnabled(false)
 
-	return &chatUI{
+	msgList := list.New([]list.Item{}, list.NewDefaultDelegate(), 400, 60)
+	msgList.Title = fmt.Sprintf("Model: %s", conf.Model)
+
+	return &model{
 		textarea:    ta,
-		messages:    []string{},
-		viewport:    vp,
+		messageList: msgList,
 		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
 		err:         nil,
+		respChan:    respChan,
 	}
 }
 
-func (c chatUI) Init() tea.Cmd {
+func (m model) Init() tea.Cmd {
 	return textarea.Blink
 }
 
-func (c chatUI) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var (
 		tiCmd tea.Cmd
 		vpCmd tea.Cmd
 	)
 
-	c.textarea, tiCmd = c.textarea.Update(msg)
-	c.viewport, vpCmd = c.viewport.Update(msg)
+	m.textarea, tiCmd = m.textarea.Update(msg)
+	m.messageList, vpCmd = m.messageList.Update(msg)
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.Type {
 		case tea.KeyCtrlC, tea.KeyEsc:
-			fmt.Println(c.textarea.Value())
-			return c, tea.Quit
-		case tea.KeyEnter:
-			input := c.textarea.Value()
-			if input == "/exit" {
-				return c, tea.Quit
-			}
-
-			messages := []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleSystem,
-					Content: "You are a helpful assistant.",
-				},
-			}
-
-			var role string
-			for i, msg := range c.messages {
-				if i%2 == 0 {
-					role = openai.ChatMessageRoleUser
-				} else {
-					role = openai.ChatMessageRoleAssistant
-				}
-				chatMsg := openai.ChatCompletionMessage{
-					Role:    role,
-					Content: msg,
-				}
-				messages = append(messages, chatMsg)
-
-			}
-			messages = append(messages, openai.ChatCompletionMessage{
-				Role:    openai.ChatMessageRoleUser,
-				Content: input,
-			})
-
-			c.messages = append(c.messages, c.senderStyle.Render("You: ")+input)
-			c.viewport.SetContent(strings.Join(c.messages, "\n"))
-			c.textarea.Reset()
-			c.viewport.GotoBottom()
-
-			resp, err := client.CreateChatCompletion(
-				context.Background(),
-				openai.ChatCompletionRequest{
-					Model:    openai.GPT3Dot5Turbo,
-					Messages: messages,
-				},
-			)
+			fmt.Println(m.textarea.Value())
+			return m, tea.Quit
+		case tea.KeyCtrlY:
+			msg := m.messageList.SelectedItem().(message)
+			err := clipboard.WriteAll(msg.Description())
 			if err != nil {
-				c.err = fmt.Errorf("ChatCompletion error: %w", err)
-				return c, nil
+				m.err = err
+				slog.Error("update error", slog.Any("error", m.err))
+				return m, nil
 			}
 
-			c.messages = append(c.messages, c.senderStyle.Render("AI: ")+resp.Choices[0].Message.Content)
-			c.viewport.SetContent(strings.Join(c.messages, "\n"))
-			c.textarea.Reset()
-			c.viewport.GotoBottom()
-		}
+			return m, nil
+		case tea.KeyEnter:
+			input := m.textarea.Value()
+			//darkMsg, err := glamour.Render(input, "dark")
+			//if err != nil {
+			//	m.err = err
+			//	slog.Error("markdown render error", slog.Any("error", m.err))
+			//	return m, nil
+			//}
 
-	// We handle errors just like any other message
+			end := len(m.messageList.Items())
+			go m.streamRequest(input, m.respChan, end+1)
+			m.textarea.Reset()
+
+			return m, tea.Batch(m.messageList.InsertItem(end, message{
+				role: openai.ChatMessageRoleUser,
+				desc: input,
+			}), m.messageList.InsertItem(end+1, message{
+				role: openai.ChatMessageRoleAssistant,
+				desc: "",
+			}))
+		}
+	case resultMsg:
+		return m, m.messageList.SetItem(msg.index, message{
+			role: openai.ChatMessageRoleAssistant,
+			desc: msg.data,
+		})
 	case error:
-		c.err = msg
-		return c, nil
+		m.err = msg
+		slog.Error("update error", slog.Any("error", m.err))
+		return m, nil
 	}
 
-	return c, tea.Batch(tiCmd, vpCmd)
+	return m, tea.Batch(tiCmd, vpCmd)
 }
 
-func (m chatUI) View() string {
+func (m *model) streamRequest(input string, respChan chan resultMsg, index int) {
+	messages := []openai.ChatCompletionMessage{
+		{
+			Role:    openai.ChatMessageRoleSystem,
+			Content: "You are a helpful assistant.",
+		},
+	}
+
+	for _, msg := range m.messageList.Items() {
+		ite := msg.(message)
+		chatMsg := openai.ChatCompletionMessage{
+			Role:    ite.role,
+			Content: ite.desc,
+		}
+		messages = append(messages, chatMsg)
+	}
+	messages = append(messages, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleUser,
+		Content: input,
+	})
+
+	stream, err := client.CreateChatCompletionStream(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model:    conf.Model,
+			Messages: messages,
+		},
+	)
+	if err != nil {
+		m.err = fmt.Errorf("chat completion error: %w", err)
+		slog.Error("chat completion error", slog.Any("error", m.err))
+		return
+	}
+
+	var content strings.Builder
+	for {
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			m.err = err
+			slog.Error("stream recv error", slog.Any("error", m.err))
+			return
+		}
+
+		content.WriteString(resp.Choices[0].Delta.Content)
+
+		//darkMsg, err := glamour.Render(content.String(), "dark")
+		//if err != nil {
+		//	m.err = err
+		//	slog.Error("markdown render error", slog.Any("error", m.err))
+		//	return
+		//}
+		respChan <- resultMsg{
+			index: index,
+			data:  content.String(),
+		}
+	}
+}
+
+func (m model) View() string {
 	return fmt.Sprintf(
 		"%s\n\n%s",
-		m.viewport.View(),
+		docStyle.Render(m.messageList.View()),
 		m.textarea.View(),
 	) + "\n\n"
 }
 
 type Config struct {
-	Endpoint string `toml:"endpoint"`
-	APIKey   string `toml:"api_key"`
-	Model    string `toml:"model"`
+	ApiUrl string `toml:"api_url"`
+	ApiKey string `toml:"api_key"`
+	Model  string `toml:"model"`
 }
 
 var (
 	conf   Config
 	client *openai.Client
-	ui     *chatUI
+	m      *model
 )
 
 func init() {
@@ -185,7 +258,9 @@ func init() {
 		log.Fatalf("Error reading YAML file: %s", err)
 	}
 
-	client = openai.NewClient(conf.APIKey)
+	config := openai.DefaultConfig(conf.ApiKey)
+	config.BaseURL = conf.ApiUrl
+	client = openai.NewClientWithConfig(config)
 }
 
 // chatCmd represents the chat command
@@ -199,10 +274,22 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		p := tea.NewProgram(initChatUI())
-
+		respChan := make(chan resultMsg, 10)
+		quitChan := make(chan struct{}, 1)
+		p := tea.NewProgram(initialModel(respChan), tea.WithAltScreen())
+		go func() {
+			for {
+				select {
+				case msg := <-respChan:
+					p.Send(msg)
+				case <-quitChan:
+					return
+				}
+			}
+		}()
 		if _, err := p.Run(); err != nil {
 			log.Fatal(err)
 		}
+		quitChan <- struct{}{}
 	},
 }
